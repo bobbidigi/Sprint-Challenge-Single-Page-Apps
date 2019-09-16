6828da78b2c4be0abcee12fd743d0d23bef8e2a4
bobbidigi <bobbidigi34@gmail.com>
2019-09-15 21:22:50 -0400

    add characters

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import CharacterList from "./CharacterList";
 
 // TODO: Add missing menu/tabs/nav below
 
@@ -10,6 +11,14 @@ import { NavLink } from "react-router-dom";
 // https://react.semantic-ui.com/elements/button/
 // https://react.semantic-ui.com/collections/breadcrumb/
 
-export default function TabNav() {
 
-};
+const panes = [
+        { menuItem: "home", pane: <CharacterList/>},
+        { menuItem: 'characters', pane: 'Tab 2 Content' },
+        { menuItem: 'locations', pane: 'Tab 3 Content' },
+        { menuItem: 'episodes', pane: 'Tab 4 Content' },
+      ]
+
+const TabNav = () => <Tab panes={panes} renderActiveOnly={false} />
+
+ export default TabNav;   
